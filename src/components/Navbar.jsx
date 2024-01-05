b26bddc81d0f3a885f76fa6b8d2f67fedb291beb
Rehan <rehanadipurwana@gmail.com>
2024-01-05 17:42:02 +0700

    feature ok

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
   return (
     <nav className="navbar bg-light border-bottom border-2 border-success shadow">
       <div className="container-fluid d-flex flex-column flex-md-row justify-content-between align-items-start align-items-md-center">
-        <Link className="navbar-brand text-success fw-bolder" to='/'>Second Nature</Link>
+        <Link className="navbar-brand text-success fw-bolder" to='/'>Kelas Food</Link>
         <ul className="nav">
           <li className="nav-item">
             <Link className="nav-link link-dark fw-semibold active" aria-current="page" to='/'>Home</Link>
